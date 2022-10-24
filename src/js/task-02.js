5d86e8c9a891e131cbfdb92847c19e0fdb50752b
NastiHluhan <0506167197more@gmail.com>
2022-10-24 16:01:00 +0300

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const pushIngredients = (arr) => {
+  const list = document.querySelector("ul");
+
+  for (const item of arr) {
+    const newEl = document.createElement("li");
+    newEl.textContent = item;
+    newEl.classList.add("item");
+
+    list.append(newEl);
+  }
+};
+
+pushIngredients(ingredients);
