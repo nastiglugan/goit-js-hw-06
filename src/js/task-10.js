c101ec840e2d0d39194462ac264678f19b4c4150
NastiHluhan <0506167197more@gmail.com>
2022-10-25 19:41:28 +0300

    task 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector("#controls input");
+const bntCreate = document.querySelector("#controls button[data-create]");
+const bntDestroy = document.querySelector("#controls button[data-destroy]");
+const boxList = document.querySelector("#boxes");
+
+let inputValue = 0;
+
+const getInputNumber = (event) => {
+  inputValue = event.currentTarget.value;
+};
+
+const createBoxes = (amount) => {
+  amount = inputValue;
+
+  for (let i = 0; i < amount; i += 1) {
+    const newBox = document.createElement("div");
+    newBox.style.width = `${30 + (i - 1) * 10}px`;
+    newBox.style.height = `${30 + (i - 1) * 10}px`;
+    newBox.style.backgroundColor = getRandomHexColor();
+
+    boxList.append(newBox);
+  }
+};
+
+const destroyBoxes = (event) => {
+  boxList.remove();
+};
+
+inputEl.addEventListener("input", getInputNumber);
+bntCreate.addEventListener("click", createBoxes);
+bntDestroy.addEventListener("click", destroyBoxes);
